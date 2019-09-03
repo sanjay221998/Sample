27d8454b130d168ddc352f99c153fb06f73e982b
SANMUGAM <SANMUGAM@DESKTOP-S2LOA84>
2019-09-03 20:29:13 +0530

    count special character

--- java basic/src/InerviewProgram/Special_char_count.java
+++ java basic/src/InerviewProgram/Special_char_count.java
@@ -6,20 +6,35 @@ public class Special_char_count {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Scanner sc=new Scanner(System.in);
-		//String s=sc.next();
-		String s="dsfjsjvbs78759087875r@#RR#";
-		char b[]=new char[s.length()];
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the string:");
+		String s=sc.next();
+		//String s="dsfjsjvbs78759087875r@#RR#";
+		char b[]=s.toCharArray();
 		int count=0;
-		for(int i=0;i<b.length;i++)
+		int coun=0;
+		
+		System.out.println(b.length);
+		/*for(int i=0;i<b.length;i++)
 		{
 			b[i]=s.charAt(i);
-		}
+		}*/
+		for(char bb:b)
+		System.out.print(bb+" ");
 		
 		for(int i=0;i<b.length;i++)
 		{
-			if(b[i])
+			if(!(((b[i]>=65)&&(b[i]<=90))||((b[i]>=97)&&(b[i]<=122))))
+			{
+				count++;
+			}
+			else
+			{
+				coun++;
+			}
 		}
+		System.out.println("\n"+count);
+		System.out.println("\n"+coun);
 		
 		
 		
