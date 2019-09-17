030c217c1dd422a7d42276ec18eb8e278f817049
SANMUGAM <SANMUGAM@DESKTOP-S2LOA84>
2019-09-17 21:01:07 +0530

    update

---
+++ GitPro/src/RarSenario/IfElseBothWorkSameTime.java
@@ -0,0 +1,17 @@
+package RarSenario;
+
+public class IfElseBothWorkSameTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(System.out.printf("\n sanjay  ")==null)
+		{
+			
+		}
+		else
+		{
+			System.out.println("\n ganesh");
+		}
+	}
+
+}
---
+++ GitPro/src/RarSenario/PrintStatementWithOutUseSemicolan.java
@@ -0,0 +1,14 @@
+package RarSenario;
+
+public class PrintStatementWithOutUseSemicolan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WITH OUT USE SEMICOLAN");
+		if(System.out.printf("sanjay")==null)
+		{
+			
+		}
+	}
+
+}
