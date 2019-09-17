b2ba27a432ecd6fac0e9b4e6c7f3db8a142dcde1
SANMUGAM <SANMUGAM@DESKTOP-S2LOA84>
2019-09-17 23:07:57 +0530

    update

--- GitPro/src/RarSenario/IfElseBothWorkSameTime.java
+++ GitPro/src/RarSenario/IfElseBothWorkSameTime.java
@@ -4,7 +4,7 @@ public class IfElseBothWorkSameTime {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if(System.out.printf("\n sanjay  ")==null)
+		if(System.out.printf("sanjay")==null)
 		{
 			
 		}
--- GitPro/src/RarSenario/PrintStatementWithOutUseSemicolan.java
+++ GitPro/src/RarSenario/PrintStatementWithOutUseSemicolan.java
@@ -2,7 +2,7 @@ package RarSenario;
 
 public class PrintStatementWithOutUseSemicolan {
 
-	public static void main(String[] args) {
+	public static void main(String[] a) {
 		// TODO Auto-generated method stub
 		System.out.println("WITH OUT USE SEMICOLAN");
 		if(System.out.printf("sanjay")==null)
