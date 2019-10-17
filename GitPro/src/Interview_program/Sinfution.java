2b90161138c0f57d8bf8fd34e4d1a16cfc32a553
sanjay221998 <54709498+sanjay221998@users.noreply.github.com>
2019-10-17 20:37:38 +0530

    Create Sinfution.java
    update

---
+++ GitPro/src/Interview_program/Sinfution.java
@@ -0,0 +1,24 @@
+package Interview_program;
+
+public class Sinfution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="sanjay";
+		char c[]=s.toCharArray();
+		int l=c.length;
+		//for(char cc:c)
+			//sSystem.out.print(cc);
+		if(l%3!=0)
+		{
+			System.out.println(s);
+		}
+		/*else {
+		for(int i=2;i<c.length;i=+3)
+		{
+			System.out.println(c[i]);
+		}
+		}*/
+	}
+
+}
