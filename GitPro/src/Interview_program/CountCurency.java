2ee27ccb9fa9437ab3ee2114f0635b6a71db5d37
sanjay221998 <54709498+sanjay221998@users.noreply.github.com>
2019-10-20 16:03:23 +0530

    Update CountCurency.java

--- GitPro/src/Interview_program/CountCurency.java
+++ GitPro/src/Interview_program/CountCurency.java
@@ -7,15 +7,20 @@ public class CountCurency {
 		int rupes[]= {2000,500,200,100,50,20,10,5,2,1};
 		int rupiscount[]=new int[rupes.length];
 		int amount=4597;
+		
 		for(int i=0;i<rupiscount.length;i++) {
 			rupiscount[i]=amount/rupes[i];
 			amount=amount-rupiscount[i]*rupes[i];
 			
 		}
 		for(int a=0;a<rupiscount.length;a++)
+		{ 
+			if(rupiscount[a]!=0)
 		{
-			System.out.println("rupise "+rupes[a]+"--------->"+"number of notes  "+rupiscount[a]);
+			System.out.println("rupise "+rupes[a]+"\t--------->"+ " number of notes  "+rupiscount[a]);
 		}
+	}
+		//System.out.println("total="+amount);
 	}
 
 }
