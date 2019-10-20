171302ad95fa6db9390539c0ed5b6279e4018da5
sanjay221998 <54709498+sanjay221998@users.noreply.github.com>
2019-10-20 16:04:21 +0530

    Create Enter0wWillStop.java

---
+++ GitPro/src/Interview_program/Enter0wWillStop.java
@@ -0,0 +1,20 @@
+package Interview_program;
+
+import java.util.Scanner;
+
+public class Enter0wWillStop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value");
+	
+		while((i=sc.nextInt())!=0)
+		{
+			i++;
+			//break;
+		}
+	}
+
+}
