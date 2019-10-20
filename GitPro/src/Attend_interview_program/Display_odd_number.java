7caabf5a6124737ecbe0fe4239d02ee601e10d0f
sanjay221998 <54709498+sanjay221998@users.noreply.github.com>
2019-10-20 16:03:38 +0530

    Create Display_odd_number.java

---
+++ GitPro/src/Attend_interview_program/Display_odd_number.java
@@ -0,0 +1,22 @@
+package Attend_interview_program;
+
+public class Display_odd_number {
+
+	public static void main(String[] args) {
+		int sum=0;
+		// TODO Auto-generated method stub
+		//int a[]= {2,7,9,5,45,87,77,56};
+		for(int i=5;i>0;i--)
+		{for(int j=0;j<i;i++)
+		{
+			System.out.print(j+" ");
+			
+		}
+		System.out.println();
+		}
+		
+		
+		
+	}
+
+}
