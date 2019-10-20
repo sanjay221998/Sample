69e82eb9cf270fefaa2e143cc63c242c326550ed
sanjay221998 <54709498+sanjay221998@users.noreply.github.com>
2019-10-20 16:12:58 +0530

    Create MissingNoWithDuplicate.java

---
+++ GitPro/src/Interview_program/MissingNoWithDuplicate.java
@@ -0,0 +1,25 @@
+package ArrayPgm;
+
+import java.util.Arrays;
+
+public class MissingNoWithDuplicate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int a[]= {10,9,6,7,5,4,3,1,2,5,6,2,1};
+		int n=a.length+1;
+		int b[]=new int[a.length];
+		a.clone();
+		System.out.println(Arrays.binarySearch(a, 4));
+		//Arrays.copyOf(a, b);*/
+		boolean a=false;
+		for(int i=1;!a;i++)
+		{
+			if(i==10)
+				a=true;
+			System.out.println("i= "+i);
+			i++;
+		}
+	}
+
+}
